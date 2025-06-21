16e0a10e15216b3355bc0492b298c7765a50c3c2
Woodaengtang <jhwoo200@gmail.com>
2025-06-22 02:33:59 +0900

    AISL

--- source/SummerProj/SummerProj/tasks/direct/franka_pap/franka_reach_env.py
+++ source/SummerProj/SummerProj/tasks/direct/franka_pap/franka_reach_env.py
@@ -144,26 +144,26 @@ class FrankaReachEnv(FrankaBaseEnv):
         action_norm = torch.norm(self.actions[:, 6:13], dim=1)
 
         # =========== Approach Reward (1): Potential Based Reward Shaping =============
-        # gamma = 1.0
-        # phi_s_prime = -self.loc_error
-        # phi_s = -self.prev_loc_error
-
-        # phi_s_prime_rot = -self.rot_error
-        # phi_s_rot = -self.prev_rot_error
-
-        # r_pos = gamma*phi_s_prime - phi_s 
-        # r_rot = gamma*phi_s_prime_rot - phi_s_rot
-
-        # =========== Approach Reward (1-1): Potential Based Reward Shaping by log scale =============
         gamma = 1.0
-        phi_s_prime = -torch.log(3 * self.loc_error + 1)
-        phi_s = -torch.log(3 * self.prev_loc_error + 1)
+        phi_s_prime = -self.loc_error
+        phi_s = -self.prev_loc_error
 
-        phi_s_prime_rot = -torch.log(3 * self.rot_error + 1)
-        phi_s_rot = -torch.log(3 * self.prev_rot_error + 1)
+        phi_s_prime_rot = -self.rot_error
+        phi_s_rot = -self.prev_rot_error
 
         r_pos = gamma*phi_s_prime - phi_s 
         r_rot = gamma*phi_s_prime_rot - phi_s_rot
+
+        # =========== Approach Reward (1-1): Potential Based Reward Shaping by log scale =============
+        # gamma = 1.0
+        # phi_s_prime = -torch.log(3 * self.loc_error + 1)
+        # phi_s = -torch.log(3 * self.prev_loc_error + 1)
+
+        # phi_s_prime_rot = -torch.log(3 * self.rot_error + 1)
+        # phi_s_rot = -torch.log(3 * self.prev_rot_error + 1)
+
+        # r_pos = gamma*phi_s_prime - phi_s 
+        # r_rot = gamma*phi_s_prime_rot - phi_s_rot
 
         # ========== Approach Reward (2): Distance Reward Shaping ===========
         # r_pos = 1 - torch.tanh(self.loc_error/3.0)
